9ec08d2b444c3d91c898f9acbe3ac1c161a7758a
willyantomaz <willyantomaz2015@gmail.com>
2024-04-28 21:20:11 -0300

    event Ok

--- src/marvel/domain/schema/event.schema.ts
+++ src/marvel/domain/schema/event.schema.ts
@@ -1,179 +1,37 @@
-import {Schema, model} from 'mongoose';	
+import { Schema, model } from 'mongoose';
 
 export const EventeSchema = new Schema({
-    type: "object",
-    properties: {
-        code: { type: "number" },
-        status: { type: "string" },
-        copyright: { type: "string" },
-        attributionText: { type: "string" },
-        attributionHTML: { type: "string" },
-        etag: { type: "string" },
-        data: {
-            type: "object",
-            properties: {
-                offset: { type: "number" },
-                limit: { type: "number" },
-                total: { type: "number" },
-                count: { type: "number" },
-                results: {
-                    type: "array",
-                    items: {
-                        type: "object",
-                        properties: {
-                            id: { type: "number" },
-                            title: { type: "string" },
-                            description: { type: "string" },
-                            resourceURI: { type: "string" },
-                            urls: {
-                                type: "array",
-                                items: {
-                                    type: "object",
-                                    properties: {
-                                        type: { type: "string" },
-                                        url: { type: "string" }
-                                    },
-                                    
-                                }
-                            },
-                            modified: { type: "string", format: "date-time" },
-                            start: { type: "string", format: "date-time" },
-                            end: { type: "string", format: "date-time" },
-                            thumbnail: {
-                                type: "object",
-                                properties: {
-                                    path: { type: "string" },
-                                    extension: { type: "string" }
-                                },
-                                
-                            },
-                            creators: {
-                                type: "object",
-                                properties: {
-                                    available: { type: "number" },
-                                    collectionURI: { type: "string" },
-                                    items: {
-                                        type: "array",
-                                        items: {
-                                            type: "object",
-                                            properties: {
-                                                resourceURI: { type: "string" },
-                                                name: { type: "string" },
-                                                role: { type: "string" }
-                                            },
-                                            
-                                        }
-                                    },
-                                    returned: { type: "number" }
-                                },
-                               
-                            },
-                            characters: {
-                                type: "object",
-                                properties: {
-                                    available: { type: "number" },
-                                    collectionURI: { type: "string" },
-                                    items: {
-                                        type: "array",
-                                        items: {
-                                            type: "object",
-                                            properties: {
-                                                resourceURI: { type: "string" },
-                                                name: { type: "string" }
-                                            },
-                                            
-                                        }
-                                    },
-                                    returned: { type: "number" }
-                                },
-                                
-                            },
-                            stories: {
-                                type: "object",
-                                properties: {
-                                    available: { type: "number" },
-                                    collectionURI: { type: "string" },
-                                    items: {
-                                        type: "array",
-                                        items: {
-                                            type: "object",
-                                            properties: {
-                                                resourceURI: { type: "string" },
-                                                name: { type: "string" },
-                                                type: { type: "string" }
-                                            },
-                                            
-                                        }
-                                    },
-                                    returned: { type: "number" }
-                                },
-                                
-                            },
-                            comics: {
-                                type: "object",
-                                properties: {
-                                    available: { type: "number" },
-                                    collectionURI: { type: "string" },
-                                    items: {
-                                        type: "array",
-                                        items: {
-                                            type: "object",
-                                            properties: {
-                                                resourceURI: { type: "string" },
-                                                name: { type: "string" }
-                                            },
-                                        
-                                        }
-                                    },
-                                    returned: { type: "number" }
-                                },
-                                
-                            },
-                            series: {
-                                type: "object",
-                                properties: {
-                                    available: { type: "number" },
-                                    collectionURI: { type: "string" },
-                                    items: {
-                                        type: "array",
-                                        items: {
-                                            type: "object",
-                                            properties: {
-                                                resourceURI: { type: "string" },
-                                                name: { type: "string" }
-                                            },
-                                            
-                                        }
-                                    },
-                                    returned: { type: "number" }
-                                },
-                               
-                            },
-                            next: {
-                                type: "object",
-                                properties: {
-                                    resourceURI: { type: "string" },
-                                    name: { type: "string" }
-                                },
-                                
-                            },
-                            previous: {
-                                type: "object",
-                                properties: {
-                                    resourceURI: { type: "string" },
-                                    name: { type: "string" }
-                                },
-                               
-                            }
-                        },
-                       
-                    }
-                }
-            },
-           
-        }
-    },
-    
-})
+   id: { type: Number },
+   title: { type: String },
+   description: { type: String },
+   resourceURI: { type: String },
+   modified: { type: String },
+   start: { type: String },
+   end: { type: String },
+   thumbnail: {
+      path: { type: String },
+      extension: { type: String }
+   },
+   creators: {
+      available: { type: Number },
+      collectionURI: { type: String },
+   },
+   characters: {
+      available:  { type: Number },
+      collectionURI: { type: String },
+   },
+   stories: {
+      available:  { type: Number },
+      collectionURI: { type: String },
+   },
+   comics: {
+      available:  { type: Number },
+      collectionURI: { type: String },
+   },
+   series: {
+      available: { type: Number },
+      collectionURI: { type: String },
+   },
+});
 
 export default model('Event', EventeSchema);
