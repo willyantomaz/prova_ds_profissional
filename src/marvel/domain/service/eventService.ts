21c1f54b28cda60b9a2aafd687730aefe3c2d567
willyantomaz <willyantomaz2015@gmail.com>
2024-05-03 10:43:00 -0300

    swagger Ok

--- src/marvel/domain/service/eventService.ts
+++ src/marvel/domain/service/eventService.ts
@@ -6,7 +6,6 @@ import { Character } from '../Entidade/charaters/character';
 
 export class EventService {
 
-<<<<<<< HEAD
     async createEvent(){
        
         const response = await fetch("http://gateway.marvel.com/v1/public/events?ts=1&apikey=7e976b6b1253c4e95653b7d83be28efe&hash=805bcb66afdc9e0448dd917b9982e90d&name=Fall of the Mutants")
@@ -20,8 +19,6 @@ export class EventService {
         return createEvent;       
         
     }       
-=======
->>>>>>> origin/gabriel
 }
 
 
