b1dc208e1927598cc37cb1de34721ab6392dc31a
gabrielAMS012 <gabriel.marassi@gmail.com>
2024-05-04 17:49:16 -0300

    Corrigindo multiplicacão de creators ao salvar no banco

--- src/marvel/domain/service/creatorService.ts
+++ src/marvel/domain/service/creatorService.ts
@@ -5,34 +5,22 @@ export class CreatorService {
 
     public async createCreator(){
        
-        const response = await fetch("http://gateway.marvel.com/v1/public/events/248/creators?ts=1&apikey=7e976b6b1253c4e95653b7d83be28efe&hash=805bcb66afdc9e0448dd917b9982e90d")
+        const response = await fetch("http://gateway.marvel.com/v1/public/events/248/creators?ts=1&apikey=7e976b6b1253c4e95653b7d83be28efe&hash=805bcb66afdc9e0448dd917b9982e90d&limit=100")
 
         const data: any = await response.json();
         let createCreator: Array<Creator> = [];       
-        let creatorResult = data.data.results;
-
-        creatorResult.forEach(async (creator: any) => {
-            let cratorMap: Creator = {
+        let creatorResult = data.data.results;    
+        await creatorResult.forEach(async (creator: any) => {
+            let creatorMap: Creator = {
                 id: creator.id,
                 nome: creator.fullName,
                 modificado: creator.modified,
                 atorURL: creator.resourceURI,
                 imagem: creator.thumbnail.path + '.' + creator.thumbnail.extension,
             }
-            createCreator.push(cratorMap);
-            creatorModel.create(createCreator);
-            console.log(cratorMap)
-            console.log(creatorResult)
-
+            createCreator.push(creatorMap);
+            creatorModel.create(creatorMap);            
         });
         return createCreator;       
-
-
-        
-   
-        
-
-
-
     }
 }
